36eb209688ac1211ba0a31e3b41c6f4356341097
haisheng <haisheng@rowenatech.com>
2019-01-08 15:31:41 +0800

    update

---
+++ script/package_tools.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+go get github.com/nightlegend/apigateway/core/grpc
+go get github.com/spf13/cobra
+go get github.com/spf13/jwalterweatherman
+go get google.golang.org/grpc
+go get github.com/spf13/cobra
